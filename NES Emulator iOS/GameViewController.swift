db990fd3b78ccb153af9671767a4c9ae446bcd8c
Kautenja <ckfour@icloud.com>
2019-05-26 15:14:32 -0500

    Build apps

---
+++ NES Emulator iOS/GameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewController.swift
+//  NES Emulator iOS
+//
+//  Created by James Kauten on 5/26/19.
+//  Copyright © 2019 Kautenja. All rights reserved.
+//
+
+import UIKit
+
+// Our iOS specific view controller
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ NES Emulator macOS/GameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewController.swift
+//  NES Emulator macOS
+//
+//  Created by James Kauten on 5/26/19.
+//  Copyright © 2019 Kautenja. All rights reserved.
+//
+
+import Cocoa
+import MetalKit
+
+// Our macOS specific view controller
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
